1afa412489142792a8f5b4a904e485146b054a96
Sardor <tuxtasinovsardor5@gmail.com>
2025-07-07 23:27:56 +0500

    uploading

---
+++ script.js
@@ -0,0 +1,12 @@
+var x, y, z, res;
+
+x = +prompt('Write your first number.')
+y = +prompt('Write your second number.')
+z = +prompt('Write your third number.')
+
+res = (x + y + z)/3
+
+console.log("The average of your three numbers are: " + res);
+
+alert("Press F12 to see the result.")
+
